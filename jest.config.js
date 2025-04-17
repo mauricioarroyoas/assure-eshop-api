8da7be52032175d94984978c252ce841118b8f7f
Mauricio Arroyo <mauricioarroyo@ascbomacmini76.local>
2025-04-17 19:06:25 -0400

    added jest config to ignore some tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    "^.+\\.tsx?$": ["ts-jest", {}],
   },
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/src/tests/product.test.ts",
+    "/src/tests/getById/not-found.test.ts",
+    "/src/tests/getById/success.test.ts",
+  ],
 };
